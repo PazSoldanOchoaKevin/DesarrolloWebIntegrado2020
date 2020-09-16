558326092533d080b6589e4200a15bccc42f7103
Kevin Paz Soldan Ochoa <kevin_pazsoldan@hotmail.com>
2020-09-16 16:24:36 -0500

    Print (ln-**)

---
+++ HelloWord/src/Hola.java
@@ -0,0 +1,10 @@
+
+public class Hola {
+	public static void main(String[] args) {
+		//Print
+		System.out.println("Hola mundo");
+		//Println salto de linea.
+		System.out.println("Hola mundo");
+	}
+
+}
